e5b9308615795d3be38b6b5d6e59122d9ab55f5d
Miguel-Liberato <142328251+Miguel-Liberato@users.noreply.github.com>
2023-09-12 09:23:02 -0500

    Create diccionario.py

---
+++ segundo corte/diccionario.py
@@ -0,0 +1,32 @@
+""" sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+
+
+translations = {"mountain":	"orod", "bread":	"bass", "friend":	"mellon", "horse":	"roch" }
+
+print(translations)
+
+subtotal_to_total = {20: 24, 10: 12, 5: 6, 15: 18}
+
+students_in_classes = {"software design": ["Aaron", "Delila", "Samson"], "cartography": ["Christopher", "Juan", "Marco"], "philosophy": ["Frederica", "Manuel"]}
+
+students_in_classes = ["philosphy"].append("marco")
+
+print(students_in_classes)
+
+students_in_classes["philosphy"].append("5")
+
+print(students_in_classes)
+
+
+
+powers = {[1, 2, 4, 8, 16]: 2, [1, 3, 9, 27, 81]: 3}
+ """
+my_empty_dictionary={}
+print(my_empty_dictionary)
+my_empty_dictionary["celulares"]=0
+print(my_empty_dictionary)
+my_empty_dictionary[3]=17
+print(my_empty_dictionary)
